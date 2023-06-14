9d9320228755d1dd10b4d0f25c7d2c817384e4f9
medi772425 <medi772425@gmail.com>
2023-06-14 20:10:08 +0900

    ローカルスコープ

--- app/Http/Controllers/PersonControlle.php
+++ app/Http/Controllers/PersonControlle.php
@@ -21,7 +21,17 @@ class PersonControlle extends Controller
     public function search(Request $request)
     {
         // リスト6-10
-        $item = Person::where('name', $request->input)->first();
+        // $item = Person::where('name', $request->input)->first();
+
+        // リスト6-12 スコープを使う Person.php の scopeNameEqualメソッドで定義したスコープ？を呼び出し
+        // 先頭のscopeが呼び出し時は不要
+        // 第一引数の$queryも不要
+        // $item = Person::nameEqual($request->input)->first();
+
+        // リスト6-14
+        $min = $request->input;
+        $max = $min + 10;
+        $item = Person::ageGreaterThan($min)->ageLessThan($max)->first();
 
         return view('person.find', ['input' => $request->input, 'item' => $item]);
     }
