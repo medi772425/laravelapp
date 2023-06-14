e137b49c183f7808add2a3a04968488b56ab33f1
medi772425 <medi772425@gmail.com>
2023-06-15 07:23:47 +0900

    グローバルスコープを作成

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,10 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model
 {
-    use HasFactory;
+    use HasFactory; // TODO これなに？
+
+
+    protected static function boot()
+    {
+        // 親のbootメソッドを実行
+        parent::boot();
+
+        // NOTEここのstatic:: の意味は、実行されるときのクラスを指しているらしい。グローバルスコープを設定するので、このクラスのグローバル的な感じか？
+        // NOTE Builderは、エロクアントのwhereを使うためのものっぽい
+        // ageの値が20以上の場合
+        static::addGlobalScope('age', function (Builder $builder) {
+            $builder->where('age', '>=', 20);
+        });
+    }
 
     public function getData()
     {
