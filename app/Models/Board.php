888e53f26b73b9140b1c44529ba2ae50a08b1a4e
medi772425 <medi772425@gmail.com>
2023-06-20 20:05:48 +0900

    belongsTo 子から親へのリレーション

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -14,8 +14,18 @@ class Board extends Model
         'message' => 'required'
     ];
 
+    public function person()
+    {
+        // sqlを確認する方法
+        // $userBuilder = $this->belongsTo(Person::class);
+        // dd($userBuilder->toSql(), $userBuilder->getBindings());
+
+        return $this->belongsTo(Person::class);
+    }
+
     public function getData()
     {
-        return $this->id . ":" . $this->title;
+        // return $this->id . ":" . $this->title;
+        return $this->id . ":" . $this->title . ' (' . $this->person->name . ')';
     }
 }
